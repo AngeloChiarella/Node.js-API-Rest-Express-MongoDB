f9396cb152289d062fe4d4cc3cce808b2899237c
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-27 16:09:11 -0300

    Metodo POST

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router // param1 Rota, param2 Acao, 
     .get("/livros", LivroController.listarLivros)
+    .post("/livros", LivroController.cadastrarLivro)
 
 export default router;
