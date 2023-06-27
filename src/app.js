16c76b70340277335a14efebd285fb133e5cfff7
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-27 13:29:28 -0300

    Implementando express e metodo post

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = new express();
 
+app.use(express.json()) // interpretar oo que chega e transformar em um obj
+
 const livros = [
     { id: 1, "titulo": "Senhor dos anéis" },
     { id: 2, "titulo": "O Hobiit" }
@@ -13,6 +15,11 @@ app.get('/', (req, res) => {
 
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro cadastrado com sucesso!')
 })
 
 export default app
