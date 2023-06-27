7181a14f52c5c739a4e580138ae4c3f9f10c0c29
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-27 10:26:38 -0300

    Servidor local e Nodemon

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http"); //utilizar modulo nativo
 const port = 3000;
 
+const rotas = { // Renderizacao de paginas
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autores',
+    '/editora' : 'Pag de editora',
+    '/sobre':'Info sobre projeto',
+};
+
 const server = http.createServer((req, res) => { //criar o servidor
     res.writeHead(200, { 'Context-type': 'text/plain' }); // cabecalho, status 200
-    res.end('Curso de Node');
+    res.end(rotas[req.url]);
 });
 
 server.listen(port, () => {
