8d6c1f6d50c5179552b143f26566d92a5ea9bdc3
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-27 14:55:43 -0300

    Iniciando com Mongoose

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
+import db from "./config/dbConnetc.js";
 
+db.on("error", console.log.bind(console, "Erro de conexao"))
+db.once("open", () => {
+    console.log("Conexao com banco feita com sucesso")
+})
 const app = new express();
 
 app.use(express.json()) // interpretar oo que chega e transformar em um obj
