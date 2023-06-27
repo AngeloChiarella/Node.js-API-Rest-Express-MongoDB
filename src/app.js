8c049e3825e30de423baa57576ca88fe0e1583ff
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-27 15:58:27 -0300

    Metodo GET

--- src/app.js
+++ src/app.js
@@ -1,29 +1,18 @@
 import express from "express";
 import db from "./config/dbConnetc.js";
 import livros from "./models/livro.js";
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, "Erro de conexao"))
 db.once("open", () => {
     console.log("Conexao com banco feita com sucesso")
 })
+
 const app = new express();
 
 app.use(express.json()) // interpretar oo que chega e transformar em um obj
 
-// const livros = [
-//     { id: 1, "titulo": "Senhor dos anéis" },
-//     { id: 2, "titulo": "O Hobiit" }
-// ]
-
-app.get('/', (req, res) => {
-    res.status(200).send("Curso de Node");
-})
-
-app.get('/livros', (req, res) => {
-    livros.find((err, livros) => {
-        res.status(200).json(livros)
-    });
-})
+routes(app);
 
 app.get('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id);
